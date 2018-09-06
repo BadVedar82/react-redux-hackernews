dedddf70f40574e3f69e30133e1146064d59b9f3
Erwin Vedar <evedar@zeal.com>
2018-09-05 20:36:12 -0700

    First commit

---
+++ src/components/Story.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Story.css';
+
+const ButtonInline = ({
+  onClick,
+  type = 'button',
+  children
+}) =>  <button
+          type="button"
+          className="button-inline"
+          onClick={onClick}
+        >
+          {children}
+        </button>
+
+const Story = ({ story, columns, onArchive }) => {
+    const {
+        title,
+        url,
+        author,
+        num_comments,
+        points,
+        objectID,
+    } = story;
+
+    return (
+        <div className="story">
+          <span style={{ width: columns.title.width }}>
+            <a href={url}>{title}</a>
+          </span>
+          <span style={{ width: columns.author.width }}>{author}</span>
+          <span style={{ width: columns.comments.width }}>{num_comments}</span>
+          <span style={{ width: columns.points.width }}>{points}</span>
+          <span style={{ width: columns.archive.width }}>
+            <ButtonInline ></ButtonInline>
+          </span>
+        </div>
+    );
+}
+
+export default Story;
