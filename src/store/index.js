9a91024076b5e07df794454bf822cad6d13712ef
Erwin Vedar <evedar@zeal.com>
2018-10-28 20:51:36 -0700

    Through Part 14

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import createSagaMIddleWare from 'redux-saga';
 import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
 const logger = createLogger();
+const saga = createSagaMIddleWare();
 
 const store = createStore(
     rootReducer,
     undefined,
-    applyMiddleware(logger)
+    applyMiddleware(saga, logger)
 )
+
+saga.run(rootSaga);
 
 export default store;
